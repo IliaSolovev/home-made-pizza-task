192881efe2f863656d76f8aff20ccf45c04eec33
Igor Shemarov <hgdrtttr@gmail.com>
2021-05-31 23:51:39 +0300

    added delivery

---
+++ ui-kit/src/delivery/delivery.stories.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Delivery } from './delivery.component';
+
+export default {
+	title: 'Delivery',
+};
+
+export const defaultDelivery = () => <Delivery>Delivery example</Delivery>;
